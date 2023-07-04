61f15b93db1297aec8a3503796cb918b78cfdb30
KrishnaYamphu <krishnayamphu@gmail.com>
2023-07-04 15:49:25 +0545

    jstl

--- src/main/java/com/ky/jstl/JstlServlet.java
+++ src/main/java/com/ky/jstl/JstlServlet.java
@@ -4,11 +4,21 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "JstlServlet", value = "/jstl")
 public class JstlServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String employees[]={"Ram","Hari","Sita"};
+        ArrayList<Book> books=new ArrayList<>();
+        books.add(new Book("C Language","Abc",200));
+        books.add(new Book("Java Language","Abc",300));
+        books.add(new Book("Web Design","Abc",150));
+        books.add(new Book("Database","Abc",260));
+
+        request.setAttribute("employees",employees);
+        request.setAttribute("books",books);
         request.getRequestDispatcher("jstl.jsp").forward(request,response);
     }
 
--- src/main/webapp/jstl.jsp
+++ src/main/webapp/jstl.jsp
@@ -6,6 +6,31 @@
 </head>
 <body>
 <c:out value="${'Welcome to JSTL'}"/>
+<%
+    int income =4000;
+%>
+<c:set var="income" value="${4000}" scope="request"/>
+<c:out value="${income}"/>
+<hr>
+<c:set var="temp" value="${40}" scope="request"/>
+<c:if test="${temp>=35}">
+    <c:out value="Too much hot"/>
+</c:if>
 
+<c:forEach var="i" begin="1" end="5">
+    <c:out value="${i}"/>
+    <br>
+</c:forEach>
+
+<hr>
+<h2>Employee List</h2>
+<c:forEach var="e" items="${employees}">
+    <p>${e}</p>
+</c:forEach>
+
+<h2>Books</h2>
+<c:forEach var="b" items="${books}">
+    <p>${b.title} - ${b.author} - ${b.price}</p>
+</c:forEach>
 </body>
 </html>
